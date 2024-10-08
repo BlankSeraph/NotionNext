adbd37d87705ad04013a219cfc27e26250acf67c
wuage <137601420@qq.com>
2024-10-08 15:29:38 +0800

    change

--- themes/commerce/config.js
+++ themes/commerce/config.js
@@ -2,13 +2,13 @@ const CONFIG = {
   // 封面大图
   COMMERCE_HOME_BANNER_ENABLE: true,
 
-  COMMERCE_TEXT_CENTER_TITLE: '产品中心', // 中间产品区块标题
+  COMMERCE_TEXT_CENTER_TITLE: 'Product Center', // 中间产品区块标题
   COMMERCE_TEXT_CENTER_DESCRIPTION:
-    '枝蔓食品是一家总部位于中国北京的纯中国本土饮料生产销售一体化公司。使命是为消费者提供健康、美味的饮料，成为生活中的选择首选，以创造更美好的生活体验。', // 中间产品区块文字描述
-  COMMERCE_TEXT_CENTER_CATEGORY_TITLE: '产品分类', // 左侧产品分类标题
-  COMMERCE_TEXT_FOOTER_TITLE: '联系我们', // COMMERCE主题页脚文案标题；按Shift+Enter键可以换行
-  COMMERCE_TEXT_FOOTER_MENU_1: '产品中心', // COMMERCE主题页脚左侧菜单标题1
-  COMMERCE_TEXT_FOOTER_MENU_2: '关于我们', // COMMERCE主题页脚左侧菜单标题2
+    'The vision of NotionNext is to help you effortlessly and seamlessly build your own website, amplifying the value of your brand.', // 中间产品区块文字描述
+  COMMERCE_TEXT_CENTER_CATEGORY_TITLE: 'Product Categories', // 左侧产品分类标题
+  COMMERCE_TEXT_FOOTER_TITLE: 'Contact US', // COMMERCE主题页脚文案标题；按Shift+Enter键可以换行
+  COMMERCE_TEXT_FOOTER_MENU_1: 'Product Center', // COMMERCE主题页脚左侧菜单标题1
+  COMMERCE_TEXT_FOOTER_MENU_2: 'About US', // COMMERCE主题页脚左侧菜单标题2
 
   COMMERCE_FOOTER_RIGHT_IMG_URL: null, // 显示页脚右侧的图片，通常放二维码
   COMMERCE_FOOTER_RIGHT_TEXT: null, // 页脚右侧图片下的文字描述
